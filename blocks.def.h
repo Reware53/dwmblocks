9cb86ad213c9205bd5e9fe2500e9a61b1c994559
potato <potato@alpine.my.domain>
2023-05-14 01:21:55 +0000

    Oi potato

--- blocks.def.h
+++ blocks.def.h
@@ -1,11 +1,18 @@
-//Modify this file to change what commands output to your statusbar, and recompile using the make command.
 static const Block blocks[] = {
-	/*Icon*/	/*Command*/		/*Update Interval*/	/*Update Signal*/
-	{"Mem:", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g",	30,		0},
-
-	{"", "date '+%b %d (%a) %I:%M%p'",					5,		0},
+    /* Icon */  /* Command */                                                                                         /* Update Interval */   /* Update Signal */
+    {" ", "free -h | awk '/^Mem/ { print $3\"/\"$2 }' | sed s/i//g", 30, 0},
+    {" ", "sh -c \"awk '/cpu / {total=\\$2+\\$3+\\$4+\\$5+\\$6+\\$7+\\$8; used=\\$2+\\$3+\\$4+\\$7+\\$8; printf(\\\"%.2f%%\\\", (used*100)/total)}' /proc/stat\"", 5, 0},
+    {" ", "df -h / | awk 'NR==2{print $4\"/\"$2}'", 60, 0},
+    {"\uf028 ", "VOL=\"$(amixer get Master | tail -n1 | sed -r 's/.*\\[(.*)%\\].*/\\1/')\" && echo \"$VOL%\"", 5, 0},
+    {" ", " date '+%b %d (%a) %I:%M%p'", 5, 0},
 };
 
-//sets delimeter between status commands. NULL character ('\0') means no delimeter.
+//sets delimiter between status commands. NULL character ('\0') means no delimiter.
 static char delim[] = " | ";
 static unsigned int delimLen = 5;
+
+static const char *fonts[] = {
+    "JetBrainsMono Nerd Font:size=10"
+};
+static const unsigned int num_fonts = sizeof(fonts) / sizeof(fonts[0]);
+
